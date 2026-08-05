8fb46700b36ac7ed11ae3643ba1fc2773fa022db
yasiru kodikara <yasirukodikara15@gmail.com>
2026-08-05 08:37:09 +0530

    Replaced the sql file with the correct sql file

---
+++ bmi.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 05, 2026 at 05:05 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bmi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bmi_data`
+--
+
+CREATE TABLE `bmi_data` (
+  `id` int(11) NOT NULL,
+  `name` char(20) DEFAULT NULL,
+  `height` float DEFAULT NULL,
+  `weight` float DEFAULT NULL,
+  `bmi` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `bmi_data`
+--
+
+INSERT INTO `bmi_data` (`id`, `name`, `height`, `weight`, `bmi`) VALUES
+(34, 'test user', 60, 160, 23.44);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bmi_data`
+--
+ALTER TABLE `bmi_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bmi_data`
+--
+ALTER TABLE `bmi_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
